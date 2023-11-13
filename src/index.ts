83fe00f84db535bbcd375c6117848fbed0bd8030
Nhuengzii <32nhueng@gmail.com>
2023-11-13 23:45:01 +0700

    set up discord.js client.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, I'm the Butler!");
+import { Client, GatewayIntentBits, IntentsBitField, TextChannel } from 'discord.js'
+import { config } from 'dotenv'
+config()
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ]
+})
+
+
+client.on('ready', () => {
+  console.log('Bot is ready')
+})
+
+const TOEKN = process.env.TOKEN
+client.login(TOEKN)
